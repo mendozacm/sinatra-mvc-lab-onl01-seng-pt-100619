921c18c647260bebda21bd0c388401dbf6b1a0f3
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-02 01:51:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
-require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
-  
+
   get '/' do
     erb :user_input
   end
-  
-  post '/' do
-    @results = PigLatinizer.new(params[:user_input])
-    erb :new_data
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
   end
-  
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,68 +1,37 @@
 class PigLatinizer
 
-  def piglatinize(user_phrase)
-    @new_word = user_phrase.split("")
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if vowels.include?(@new_word[0])
-      latinized_word = begins_with_vowel.join("")
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+    # word starts with vowel
+    if !consonant?(word[0])
+      word = word + "w"
+    # word starts with 3 consonants
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+    # word starts with 2 consonants
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+    # word starts with 1 consonant
     else
-      latinized_word = begins_with_consonant.join("")
+      word = word.slice(1..-1) + word.slice(0)
     end
-    latinized_word
+    word << "ay"
   end
 
-  def to_pig_latin(sentence)
-    words_in_sentence = sentence.split(" ")
-
-    pig_latinized_words = words_in_sentence.collect do |word|
-      self.piglatinize(word)
-    end
-
-    new_sentence = pig_latinized_words.join(" ")
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
 
-  def begins_with_vowel
-    vowel_word = @new_word << "way"
-  end
 
-  def begins_with_consonant
-    if @new_word[0] == "q" && @new_word[1] == "u"
-      qu = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << qa + "ay"
-    elsif @new_word[0] == "p" && @new_word[1] =="l"
-      pl = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << pl + "ay"
-    elsif @new_word[0] == "t" && @new_word[1] =="h"
-      th = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << th + "ay"
-    elsif @new_word[0] == "s" && @new_word[1] =="p" && @new_word[2] == "r"
-      spr = @new_word[0, 3].join("")
-      spliced_word = @new_word[3..-1]
-      spliced_word << spr + "ay"
-    elsif @new_word[0] == "p" && @new_word[1] =="r"
-      pr = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << pr + "ay"
-    elsif @new_word[0] == "w" && @new_word[1] =="h"
-      wh = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << wh + "ay"
-    elsif @new_word[0] == "s" && @new_word[1] =="k"
-      sk = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << sk + "ay"
-    elsif (@new_word[0] == "s" || @new_word[0] == "S") && @new_word[1] =="t" && @new_word[2] == "r"
-      str = @new_word[0, 3].join("")
-      spliced_word = @new_word[3..-1]
-      spliced_word << str + "ay"
-    else
-      first_letter = @new_word[0]
-      spliced_word = @new_word[1..-1]
-      spliced_word << first_letter + "ay"
-    end
-  end
+
 
 end
