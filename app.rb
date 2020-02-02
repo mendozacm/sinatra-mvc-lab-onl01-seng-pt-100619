232edcff63802790d7bbc263172c8f9eadfc1ef9
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-02 01:44:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/' do
+    @results = PigLatinizer.new(params[:user_input])
+    erb :new_data
+  end
+  
 end
